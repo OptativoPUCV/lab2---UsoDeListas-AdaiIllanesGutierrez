2cb5666487f0eb9ea22f96f228541fb9f1394786
Your Name <you@example.com>
2024-03-21 20:28:43 +0000

    new try

--- exercises.c
+++ exercises.c
@@ -135,6 +135,7 @@ paraéntesis balanceados. Retorna 1 si están balanceados,
 
 int parentesisBalanceados(char *cadena) {
   int balance = 0;
+  //crear pila ir guardando 
   for (int i = 0; cadena[i] != '\0'; i++) {
       if (cadena[i] == '('||cadena[i] == '['||cadena[i] == '{') {
          balance++;
